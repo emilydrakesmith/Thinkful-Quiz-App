770ec3fbd69c7009c6f0053828bd746c656e2dd5
Marty Smith <marty.smith01@yahoo.com>
2020-09-29 12:51:21 -0400

    submit button found 2

--- app.js
+++ app.js
@@ -1,36 +1,4 @@
 /**
- * Example store structure
- */
-const store = {
-  // 5 or more questions are required
-  questions: [
-    {
-      question: 'What color is broccoli?',
-      answers: [
-        'red',
-        'orange',
-        'pink',
-        'green'
-      ],
-      correctAnswer: 'green'
-    },
-    {
-      question: 'What is the current year?',
-      answers: [
-        '1970',
-        '2015',
-        '2019',
-        '2005'
-      ],
-      correctAnswer: '2019'
-    }
-  ],
-  quizStarted: false,
-  questionNumber: 0,
-  score: 0
-};
-
-/**
  * 
  * Technical requirements:
  * 
@@ -47,12 +15,113 @@ const store = {
 
 /********** TEMPLATE GENERATION FUNCTIONS **********/
 
-// These functions return HTML templates
+let n = 0;
+let currAnswers = [];
+
+function genWelcome() {
+  return `<section class="quiz-box">
+            <h4>Welcome to your quiz!</h4>
+            <button id="start-btn">Get Started</button>
+          </section>`;
+}
+
+function genQuiz() {
+  getCurrentAnswers();               /* I need to figure out a way to get this to re-run every time the question is updated. */
+  return `<section class="quiz-box">
+            
+
+          <form id="quiz-form" class="quiz-box">
+              <fieldset>
+                <div class="question">
+                  <legend style="color:black;"><h2>Question ${n+1}</h2><h3>${quiz.questions[n].question}</h3></legend>
+                </div>
+                <div class="answers">                                                                       
+                  <button type="button" id='choice1' class="answer-choice raw-answer">${currAnswers[0]}</button>
+                  <button type="button" id='choice2' class="answer-choice raw-answer">${currAnswers[1]}</button>
+                  <button type="button" id='choice3' class="answer-choice raw-answer">${currAnswers[2]}</button>
+                  <button type="button" id='choice4' class="answer-choice raw-answer">${currAnswers[3]}</button>
+                </div>
+                <div class="quiz-buttons">
+                  <button type="submit" id="submit-answer-btn">Submit Answer</button>
+                  <button type="button" id="next-question-btn">Next Question</button>
+                </div>
+              </fieldset>
+            </form >
+
+
+          </section>
+          <section>
+            <p>You have answered # out of # questions correctly.</p>
+            <p>There are ${10 - n} questions remaining.</p>
+          </section>`;
+}
+
+function getCurrentAnswers() {
+  currAnswers = quiz.questions[n].answers;
+  console.log(currAnswers);
+  return currAnswers;
+}
+
+
 
 /********** RENDER FUNCTION(S) **********/
 
-// This function conditionally replaces the contents of the <main> tag based on the state of the store
+function renderWelcome() {
+  if (quiz.quizStarted === false) {
+    return $('main').html(genWelcome());
+  } 
+}
+
+function renderQuiz() {
+    if (quiz.quizStarted === true) {
+    return $('main').html(genQuiz())
+  }
+}
 
 /********** EVENT HANDLER FUNCTIONS **********/
 
-// These functions handle events (submit, click, etc)
+function launchQuiz() {
+  $('body').on('click', '#start-btn', function (event) {
+    quiz.quizStarted = true;
+    renderQuiz();
+  })
+}
+
+function chooseAnswer() {                              
+  $('body').on('mousedown', '.answer-choice', function(event) {
+    if ($(this).hasClass('raw-answer')) {
+      $(this).addClass('selected-answer')
+      $(this).siblings().removeClass('selected-answer');
+    }
+  })
+  /*   now we need to code the submit button to
+   *   check if the answer is correct, color the 
+   *   answers red/green as warranted, and append
+   *   the results to the counter
+   * 
+   *   
+   */
+}
+
+function clickSubmit() {
+  $('body').on('submit', '#quiz-form', function(event) {
+    event.preventDefault();
+    let x = $('.selected-answer');
+    
+    if (x.text() === quiz.questions[n].correctAnswer) {
+      console.log ('hooray')
+    } else {
+      console.log('boo')
+    }
+    
+  })
+}
+
+function initializePage() {
+  renderWelcome();
+  launchQuiz();
+  chooseAnswer();
+  clickSubmit();
+}
+
+$(initializePage)
---
+++ quiz.js
@@ -0,0 +1,108 @@
+const quiz = {
+    // 5 or more questions are required
+    questions: [
+      { /* Question 1 */
+        question: 'What term refers to the cheese-like necrosis seen in Tuberculosis patients?',
+        answers: [
+          'caseous necrosis',
+          'fibrinoid necrosis',
+          'coagulative necrosis',
+          'gangrenous necrosis'
+        ],
+        correctAnswer: 'caseous necrosis'
+      },
+      { /* Question 2 */
+        question: 'What process involves heat-treating food to kill pathogens?',
+        answers: [
+          'pasteurization',
+          'fever-induction',
+          'pyrotherapy',
+          'autoclaving'
+        ],
+        correctAnswer: 'pasteurization'
+      },
+      { /* Question 3 */
+        question: 'What process involves heat-treating food to kill pathogens?',
+        answers: [
+          'pasteurization',
+          'fever-induction',
+          'pyrotherapy',
+          'autoclaving'
+        ],
+        correctAnswer: 'pasteurization'
+      },
+      { /* Question 4 */
+        question: 'What process involves heat-treating food to kill pathogens?',
+        answers: [
+          'pasteurization',
+          'fever-induction',
+          'pyrotherapy',
+          'autoclaving'
+        ],
+        correctAnswer: 'pasteurization'
+      },
+      { /* Question 5 */
+        question: 'What process involves heat-treating food to kill pathogens?',
+        answers: [
+          'pasteurization',
+          'fever-induction',
+          'pyrotherapy',
+          'autoclaving'
+        ],
+        correctAnswer: 'pasteurization'
+      },
+      { /* Question 6 */
+        question: 'What process involves heat-treating food to kill pathogens?',
+        answers: [
+          'pasteurization',
+          'fever-induction',
+          'pyrotherapy',
+          'autoclaving'
+        ],
+        correctAnswer: 'pasteurization'
+      },
+      { /* Question 7 */
+        question: 'What process involves heat-treating food to kill pathogens?',
+        answers: [
+          'pasteurization',
+          'fever-induction',
+          'pyrotherapy',
+          'autoclaving'
+        ],
+        correctAnswer: 'pasteurization'
+      },
+      { /* Question 8 */
+        question: 'What process involves heat-treating food to kill pathogens?',
+        answers: [
+          'pasteurization',
+          'fever-induction',
+          'pyrotherapy',
+          'autoclaving'
+        ],
+        correctAnswer: 'pasteurization'
+      },
+      { /* Question 9 */
+        question: 'What process involves heat-treating food to kill pathogens?',
+        answers: [
+          'pasteurization',
+          'fever-induction',
+          'pyrotherapy',
+          'autoclaving'
+        ],
+        correctAnswer: 'pasteurization'
+      },
+      { /* Question 10 */
+        question: 'What process involves heat-treating food to kill pathogens?',
+        answers: [
+          'pasteurization',
+          'fever-induction',
+          'pyrotherapy',
+          'autoclaving'
+        ],
+        correctAnswer: 'pasteurization'
+      }
+    ],
+    quizStarted: false,
+    questionNumber: 0,
+    score: 0
+  };
