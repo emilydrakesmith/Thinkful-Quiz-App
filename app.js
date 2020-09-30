bb4d575c4ed0266104d7534df9f7e7f434b35557
Marty Smith <marty.smith01@yahoo.com>
2020-09-29 20:08:24 -0400

    nearly done just needs proofing

--- app.js
+++ app.js
@@ -34,11 +34,9 @@ function genWelcome() {
 }
 
 function genQuiz() {
-  getCurrentAnswers();
+  getCurrentAnswers();      /* this function has to run here for answers to be available below */
   return `<section class="quiz-box">
-            
-
-          <form id="quiz-form" class="quiz-box">
+          <form id="quiz-form">
               <fieldset>
                 <div class="question">
                   <legend style="color:black;"><h2>Question ${n+1}</h2><h3>${quiz.questions[n].question}</h3></legend>
@@ -50,34 +48,38 @@ function genQuiz() {
                   <button type="button" id='choice4' class="answer-choice raw-answer">${currAnswers[3]}</button>
                 </div>
                 <div class="quiz-buttons">
-                  <button type="submit" id="submit-answer-btn">Submit Answer</button>
-                  <button type="button" id="next-question-btn" class="btn-hide">Next Question</button>
+                  <button type="submit" id="submit-answer-btn" disabled="disabled">Submit Answer</button>
+                  <button type="button" id="next-question-btn" class="hide">Next Question</button>
                 </div>
               </fieldset>
             </form >
-
-
           </section>
           <section>
-            <p class="running-score">You have answered ${numCorrect} out of 0 questions correctly.</p>
-            <p>There are ${10-n} questions remaining.</p>
+            <p class="running-score">You have answered ${correctCount} of ${n} questions correctly.</p>
+            <p class ="hide">There are ${9-n} questions remaining.</p>
           </section>`;
 }
 
-
+function genResults() {
+  return `<section class="quiz-box">
+            <h2>You finished!</h2>
+            <h3>Your score is: ${correctCount} of ${n}.<h3>
+            <button id="try-again-btn">Try Again</button>
+          </section>`;
+}
 
 /********** RENDER FUNCTION(S) **********/
 
 function renderWelcome() {
-  if (quiz.quizStarted === false) {
     return $('main').html(genWelcome());
-  } 
 }
 
 function renderQuiz() {
-    if (quiz.quizStarted === true) {
-    return $('main').html(genQuiz())
-  }
+  return $('main').html(genQuiz());
+}
+
+function renderResults() {
+  return $('main').html(genResults());
 }
 
 /********** EVENT HANDLER FUNCTIONS **********/
@@ -86,26 +88,25 @@ function launchQuiz() {
   $('body').on('click', '#start-btn', function (event) {
     quiz.quizStarted = true;
     renderQuiz();
-    chooseAnswer();
-    clickSubmit();
-    clickNext();
+
   })
 }
 
 function chooseAnswer() {                              
   $('body').on('mousedown', '.answer-choice', function(event) {
     if ($(this).hasClass('raw-answer')) {
-      $(this).addClass('selected-answer')
-      $(this).siblings().removeClass('selected-answer');
+      $(this).addClass('selected-answer answer-border')
+      $(this).siblings().removeClass('selected-answer answer-border');
+      $('#quiz-form').find(':button[type=submit]').prop('disabled', false)
     }
   })
-  /*   now we need to code the submit button to
-   *   check if the answer is correct, color the 
-   *   answers red/green as warranted, and append
-   *   the results to the counter
-   * 
-   *   
-   */
+  $('body').on('keydown', '.answer-choice', function(event) {
+    if ($(this).hasClass('raw-answer')  && event.keyCode === 32  ) {
+      $(this).addClass('selected-answer answer-border')
+      $(this).siblings().removeClass('selected-answer answer-border');
+      $('#quiz-form').find(':button[type=submit]').prop('disabled', false)
+    }
+  })
 }
 
 function clickSubmit() {
@@ -116,12 +117,9 @@ function clickSubmit() {
     $(this).find(':button[type=submit]').prop('disabled', true)
     if (x.text() === quiz.questions[n].correctAnswer) {
       correctCount ++;
-      console.log(correctCount)
-    } else {
-      console.log('boo')
     }
     $(this).find('.answer-choice')
-    for (i=1; i<=4; i++) {
+    for (i=1; i<=quiz.questions[n].question.length; i++) {
       let choice = 'choice' + i;
       if ($(`#${choice}`).text() === quiz.questions[n].correctAnswer) {
         $(`#${choice}`).addClass('correct-answer');
@@ -129,10 +127,9 @@ function clickSubmit() {
         $(`#${choice}`).addClass('wrong-answer');
       }
     }
-    $('#next-question-btn').removeClass('btn-hide');
+    $('.hide').removeClass('hide');
     increaseCorrectCount();
     n++;
-    console.log(quiz.length)
   })
 }
 
@@ -143,10 +140,28 @@ function increaseCorrectCount() {
 function clickNext() {
   $('body').on('click', '#next-question-btn', function(event) {
     if (n<quiz.questions.length){
-      genQuiz();
+      renderQuiz();
+    } else {
+      renderResults();
+      console.log('tada');
     }
-    console.log('tada');
+    
   })
+}
+
+function clickTryAgain() {
+  $('body').on('click', '#try-again-btn', function (event) {
+    quiz.quizStarted = false;
+    n=0;
+    currAnswers = [];
+    correctCount = 0;
+    numCorrect = 0;
+    renderWelcome();
+  })
+}
+
+function removePriorResults() {
+  $('.answer-choice').removeClass('correct-answer wrong-answer');
 }
 
 
@@ -155,6 +170,10 @@ function clickNext() {
 function initializePage() {
   renderWelcome();
   launchQuiz();
+  chooseAnswer();
+  clickSubmit();
+  clickNext();
+  clickTryAgain();
 }
 
 $(initializePage)
--- quiz.js
+++ quiz.js
@@ -4,9 +4,9 @@ const quiz = {
       { /* Question 1 */
         question: 'What term refers to the cheese-like necrosis seen in Tuberculosis patients?',
         answers: [
-          'caseous necrosis',
-          'fibrinoid necrosis',
           'coagulative necrosis',
+          'fibrinoid necrosis',
+          'caseous necrosis',
           'gangrenous necrosis'
         ],
         correctAnswer: 'caseous necrosis'
@@ -22,84 +22,84 @@ const quiz = {
         correctAnswer: 'pasteurization'
       },
       { /* Question 3 */
-        question: 'What process involves heat-treating food to kill pathogens?',
+        question: 'Which of the following are viruses that only infect bacterial cells?',
         answers: [
-          'pasteurization',
-          'fever-induction',
-          'pyrotherapy',
-          'autoclaving'
+          'viroids',
+          'bacteriophages',
+          'virions',
+          'heliobacters'
         ],
-        correctAnswer: 'pasteurization'
+        correctAnswer: 'bacteriophages'
       },
       { /* Question 4 */
-        question: 'What process involves heat-treating food to kill pathogens?',
+        question: 'What are the five antibody isotypes?',
         answers: [
-          'pasteurization',
-          'fever-induction',
-          'pyrotherapy',
-          'autoclaving'
+          'IgA  IgD  IgE  IgG  IgM',
+          'IgA  IgB  IgD  IgE  IgR',
+          'IgA  IgG  IgM  IgR  IgS',
+          'IgB  IgD  IgG  IgG  IgS'
         ],
-        correctAnswer: 'pasteurization'
+        correctAnswer: 'IgA  IgD  IgE  IgG  IgM'
       },
       { /* Question 5 */
-        question: 'What process involves heat-treating food to kill pathogens?',
+        question: 'What type of cellular respiration does NOT require oxygen?',
         answers: [
-          'pasteurization',
-          'fever-induction',
-          'pyrotherapy',
-          'autoclaving'
+          'oligophobic',
+          'aerobic',
+          'exerobic',
+          'anaerobic'
         ],
-        correctAnswer: 'pasteurization'
+        correctAnswer: 'anaerobic'
       },
       { /* Question 6 */
-        question: 'What process involves heat-treating food to kill pathogens?',
+        question: 'Which of the following is NOT one of the four signs of inflammation?',
         answers: [
-          'pasteurization',
-          'fever-induction',
-          'pyrotherapy',
-          'autoclaving'
+          'redness',
+          'heat',
+          'numbness',
+          'swelling'
         ],
-        correctAnswer: 'pasteurization'
+        correctAnswer: 'numbness'
       },
       { /* Question 7 */
-        question: 'What process involves heat-treating food to kill pathogens?',
+        question: 'Which specialized immune cells are only found in the gut?',
         answers: [
-          'pasteurization',
-          'fever-induction',
-          'pyrotherapy',
-          'autoclaving'
+          'neutrophils, macrophages, B-1 cells',
+          'effector B cells, T(h)17 cells, NK cells',
+          'T(reg) cells, eosinophils, monocytes',
+          'M cells, crypt cells, goblet cells'
         ],
-        correctAnswer: 'pasteurization'
+        correctAnswer: 'M cells, crypt cells, goblet cells'
       },
       { /* Question 8 */
-        question: 'What process involves heat-treating food to kill pathogens?',
+        question: 'What is the primary short-term energy storage molecule in animals?',
         answers: [
-          'pasteurization',
-          'fever-induction',
-          'pyrotherapy',
-          'autoclaving'
+          'adenosine triphosphate (ATP)',
+          'adenosine diphosphate (ADP)',
+          'guanosine diphosphate (GDP)',
+          'guanosine monophosphate (GMP)'
         ],
-        correctAnswer: 'pasteurization'
+        correctAnswer: 'adenosine triphosphate (ATP)'
       },
       { /* Question 9 */
-        question: 'What process involves heat-treating food to kill pathogens?',
+        question: 'What mineral in hemoglobin allows blood to bind oxygen in humans?',
         answers: [
-          'pasteurization',
-          'fever-induction',
-          'pyrotherapy',
-          'autoclaving'
+          'copper (Cu)',
+          'iron (Fe)',
+          'zinc (Zn)',
+          'nickel (Ni)'
         ],
-        correctAnswer: 'pasteurization'
+        correctAnswer: 'iron (Fe)'
       },
       { /* Question 10 */
-        question: 'What process involves heat-treating food to kill pathogens?',
+        question: 'What process causes the planned death of a cell?',
         answers: [
-          'pasteurization',
-          'fever-induction',
-          'pyrotherapy',
-          'autoclaving'
+          'anaphase',
+          'autophagy',
+          'apoptosis',
+          'anergy'
         ],
-        correctAnswer: 'pasteurization'
+        correctAnswer: 'apoptosis'
       }
     ],
     quizStarted: false,
